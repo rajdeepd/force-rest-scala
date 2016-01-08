5dae100849ca09c7b9b437f586fb36d2a5471f4b
Rajdeep Dua <dua_rajdeep@yahoo.com>
2016-01-08 17:21:45 +0530

    removed unwanted code

--- src/main/scala/createAccount.scala
+++
@@ -1,8 +0,0 @@
-object CreateAccount{
-	    def main(args: Array[String]): Unit = {
-	    	val sObject = new SObject("Account")
-	    	val json = """{"name":"Account1"}"""
-	    	sObject.createSObject(json)
-			//println(body)
-	    }
-}
